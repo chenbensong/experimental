fd64ce09a2e82a628429b2cabf34f25813733537
chenbensong <chenbensong@gmail.com>
2014-09-19 19:23:20 -0400

    Update size.js

--- size/size.js
+++ size/size.js
@@ -1,3 +1,38 @@
 /**
  * Size Plot Interactions.
  */
+(function() {
+  /**
+   * $$ returns a real JS array of DOM elements that match the CSS query selector.
+   *
+   * A shortcut for jQuery-like $ behavior.
+   **/
+  function $$(query, ele) {
+    if (!ele) {
+      ele = document;
+    }
+    return Array.prototype.map.call(ele.querySelectorAll(query), function(e) { return e; });
+  }
+
+  /**
+   * $$$ returns the DOM element that match the CSS query selector.
+   *
+   * A shortcut for document.querySelector.
+   **/
+  function $$$(query, ele) {
+    if (!ele) {
+      ele = document;
+    }
+    return ele.querySelector(query);
+  }
+  
+  function onLoad() {
+    $$$('#plot')..innerText = 'Here goes plot.';
+  }
+  
+  if (document.readyState != 'loading') {
+    onLoad();
+  } else {
+    window.addEventListener('load', onLoad);
+  }
+})();
